7c3f36eee384917b731e2699104a0481889c07ad
Elvin Lin <ssfvessfve@163.com>
2023-09-13 09:52:13 +0800

    Update chatgpt4.py

--- chatgpt4.py
+++ chatgpt4.py
@@ -27,7 +27,7 @@ def generate_response(prompt):
     oneLine["content"] = pruneText(prompt.content, 1000)
     userCon.append(oneLine)
     conDict[key] = userCon
-    print("userCon=" + userCon)
+    print(userCon)
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=userCon, temperature=0, max_tokens=2048,
                                             top_p=1, frequency_penalty=0.0, presence_penalty=0.0)
     message = response.choices[0].message.content.strip()
