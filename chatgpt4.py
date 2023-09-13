faa117042f0de6f6cb98c3eaca359e0213e9d780
Elvin Lin <ssfvessfve@163.com>
2023-09-13 09:31:36 +0800

    Update chatgpt4.py

--- chatgpt4.py
+++ chatgpt4.py
@@ -4,20 +4,21 @@ import openai
 import threading
 
 ##openai.api_key="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"
-openai.api_key="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"
-app_id_str="wx7b2e7378fb505046"
-app_secret_str="a59032da3bc54b9b46e66a08fdc12e3c"
-#client = werobot.Client(appid, app_secret)
+openai.api_key = "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"
+app_id_str = "wx7b2e7378fb505046"
+app_secret_str = "a59032da3bc54b9b46e66a08fdc12e3c"
+# client = werobot.Client(appid, app_secret)
 robot = werobot.WeRoBot(token='123456', app_id=app_id_str, app_secret=app_secret_str)
 cacheDict = {}
-conDict={}
+conDict = {}
+
 
 def generate_response(prompt):
     ##response = openai.Completion.create(model="text-davinci-003",prompt=prompt.content,temperature=0,max_tokens=1024,top_p=1,frequency_penalty=0.0,presence_penalty=0.0)
     userCon = []
     key = prompt.source
     print(key)
-    if(key in conDict):
+    if (key in conDict):
         userCon = conDict[key]
         print(userCon)
     oneLine = {}
@@ -26,11 +27,13 @@ def generate_response(prompt):
     userCon.append(oneLine)
     conDict[key] = userCon
     print(userCon)
-    response = openai.ChatCompletion.create(model="gpt-4",messages=userCon,temperature=0,max_tokens=2048,top_p=1,frequency_penalty=0.0,presence_penalty=0.0)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=userCon, temperature=0, max_tokens=2048,
+                                            top_p=1, frequency_penalty=0.0, presence_penalty=0.0)
     message = response.choices[0].message.content.strip()
     print(message)
-    cut_res = '[' + pruneText(prompt.content, 35)  + ']\r\n' + pruneText(message, 2000) + '[AI]'
+    cut_res = '[' + pruneText(prompt.content, 35) + ']\r\n' + pruneText(message, 2000) + '[AI]'
     robot.client.send_text_message(key, cut_res)
+
 
 def pruneText(text, length):
     cut_bytes = text.encode('utf-8')
@@ -38,8 +41,9 @@ def pruneText(text, length):
     cut_res = cut_tmp.decode('utf-8', errors='ignore')
     return cut_res
 
+
 @robot.text
-def hello (message):
+def hello(message):
     key = message.content
     print(key)
     t = threading.Thread(target=generate_response, args=(message,))
@@ -47,9 +51,11 @@ def hello (message):
     ##t.join()
     return
 
+
 @robot.subscribe
-def new_user (message):
+def new_user(message):
     return '您好，说出你的问题吧，与桌游相关或无关皆可'
+
 
 robot.config['HOST'] = '0.0.0.0'
 ##robot.config['HOST']='34.72.162.21'
